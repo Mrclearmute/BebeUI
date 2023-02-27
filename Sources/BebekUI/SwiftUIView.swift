7701f165c7519e1c00eea06e33565068ddc809fd
mrclearmute <mrclearmute1@gmail.com>
2023-02-27 17:25:26 +0100

    Bu güncelleme daha fazla özellik eklendi.

--- Sources/BebekUI/SwiftUIView.swift
+++ Sources/BebekUI/SwiftUIView.swift
@@ -11,23 +11,44 @@ import SwiftUI
 @frozen public struct SwiftUIView: View, Bebek{
     public var meslek: Meslek
     public var isim: String
-    public var logo: Image
+    public var image: Image?
+    public var width: CGFloat?
+    public var heigth: CGFloat?
     public var body: some View {
-        Account(isim: isim, logo: logo, width: 100, heigth: 100, font: .title)
+        Account(isim: isim, logo: image!, width: width, heigth: width, font: .title)
     }
     public func hash(into hasher: inout Hasher) {
         hasher.combine(isim)
     }
-    @inlinable public init(meslek: Meslek, isim: String, logo: Image) {
+    @inlinable public init(meslek: Meslek, isim: String) {
         self.meslek = meslek
         self.isim = isim
-        self.logo = logo
+        
+        switch self.isim.lowercased(){
+        case "Bebe Kalkanatik Devreleri".lowercased():
+            self.image = Image(systemName: "shield.lefthalf.fill")
+        default:
+            self.image = Image(systemName: "person.crop.circle.fill.badge.questionmark")
+        }
+    }
+    @inlinable public init(meslek: Meslek, isim: String, width: CGFloat, height: CGFloat) {
+        self.meslek = meslek
+        self.isim = isim
+        self.width = width
+        self.heigth = height
+        
+        switch self.isim.lowercased(){
+        case "Bebe Kalkanatik Devreleri".lowercased():
+            self.image = Image(systemName: "shield.lefthalf.fill")
+        default:
+            self.image = Image(systemName: "person.crop.circle.fill.badge.questionmark")
+        }
     }
 }
 
 @available(macOS 11.0, iOS 14.0, tvOS 14.0, watchOS 4.0, *)
 struct SwiftUIView_Previews: PreviewProvider {
     static var previews: some View {
-        SwiftUIView(meslek: .ajan, isim: "Bebe", logo: Image(systemName: "person.crop.circle.fill"))
+        SwiftUIView(meslek: .ajan, isim: "Bebe", width: 100, height: 100)
     }
 }
