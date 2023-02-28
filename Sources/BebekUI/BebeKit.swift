e0e08a2a0d288e4d2e45fd57577d31ef39f5b2ad
mrclearmute <mrclearmute1@gmail.com>
2023-02-28 16:29:43 +0100

    Bu bazı UI iyiileştirmeleri ve test örnekleri bulunuyor.

--- Sources/BebekUI/BebeKit.swift
+++ Sources/BebekUI/BebeKit.swift
@@ -1,26 +1,28 @@
 import Foundation
 import SwiftUI
 
-@available(macOS 10.15, iOS 14.0, tvOS 14.0, watchOS 4.0, *)
+@available(macOS 11.0, iOS 14.0, tvOS 14.0, watchOS 4.0, *)
 @frozen public struct Account: View, Equatable, Hashable{
-    var isim: String
-    var logo: Image
-    var width: CGFloat?
-    var heigth: CGFloat?
-    var font: Font?
+    public var isim: String
+    public var logo: Image
+    public var font: Font
+    public var textfont: Font
     public var body: some View{
         VStack {
             logo
-                .resizable()
-                .frame(width: width, height: heigth)
-            Text(isim)
                 .font(font)
+            Text(isim)
+                .font(textfont)
         }
+    }
+    @inlinable public init(isim: String, logo: Image, font: Font, textfont: Font) {
+        self.isim = isim
+        self.logo = logo
+        self.font = font
+        self.textfont = textfont
     }
     public func hash(into hasher: inout Hasher) {
         hasher.combine(isim)
-        hasher.combine(width)
-        hasher.combine(heigth)
     }
     public static func == (a: Account, b: Account) -> Bool{
         a.hashValue == b.hashValue
--- Sources/BebekUI/SwiftUIView.swift
+++ Sources/BebekUI/SwiftUIView.swift
@@ -11,11 +11,12 @@ import SwiftUI
 @frozen public struct SwiftUIView: View, Bebek{
     public var meslek: Meslek
     public var isim: String
-    public var image: Image?
-    public var width: CGFloat?
-    public var heigth: CGFloat?
+    public var image: Image
+    public var font: Font
+    public var textfont: Font
+    public var kalkan = false
     public var body: some View {
-        Account(isim: isim, logo: image!, width: width, heigth: width, font: .title)
+        Account(isim: isim, logo: image, font: font, textfont: textfont)
     }
     public func hash(into hasher: inout Hasher) {
         hasher.combine(isim)
@@ -23,6 +24,8 @@ import SwiftUI
     @inlinable public init(meslek: Meslek, isim: String) {
         self.meslek = meslek
         self.isim = isim
+        self.font = .callout
+        self.textfont = .body
         
         switch self.isim.lowercased(){
         case "Bebe Kalkanatik Devreleri".lowercased():
@@ -31,16 +34,43 @@ import SwiftUI
             self.image = Image(systemName: "person.crop.circle.fill.badge.questionmark")
         }
     }
-    @inlinable public init(meslek: Meslek, isim: String, width: CGFloat, height: CGFloat) {
+    @inlinable public init(meslek: Meslek, isim: String, font: Font, textfont: Font) {
         self.meslek = meslek
         self.isim = isim
-        self.width = width
-        self.heigth = height
+        self.font = font
+        self.textfont = textfont
+        let ad = Array(self.isim.lowercased())
+        var devam = false
+        var sayı = 0
+        kalkan = false
+        for harf in ad{
+            if harf == "k"{
+                devam = true
+                sayı += 1
+            } else if harf == "a" && devam == true{
+                sayı += 1
+            } else if harf == "l" && devam == true{
+                sayı += 1
+            } else if harf == "k" && devam == true{
+                sayı += 1
+            } else if harf == "a" && devam == true{
+                sayı += 1
+            } else if harf == "n" && devam == true{
+                sayı += 1
+                print(harf)
+            } else {
+                if sayı == 6 && devam == true{
+                    kalkan = true
+                }
+                devam = false
+            }
+        }
         
-        switch self.isim.lowercased(){
-        case "Bebe Kalkanatik Devreleri".lowercased():
+        if self.isim.lowercased() == "Bebe Kalkanatik Devreleri".lowercased(){
             self.image = Image(systemName: "shield.lefthalf.fill")
-        default:
+        } else if kalkan == true{
+            self.image = Image(systemName: "exclamationmark.shield.fill")
+        } else {
             self.image = Image(systemName: "person.crop.circle.fill.badge.questionmark")
         }
     }
@@ -49,6 +79,7 @@ import SwiftUI
 @available(macOS 11.0, iOS 14.0, tvOS 14.0, watchOS 4.0, *)
 struct SwiftUIView_Previews: PreviewProvider {
     static var previews: some View {
-        SwiftUIView(meslek: .ajan, isim: "Bebe", width: 100, height: 100)
+        SwiftUIView(meslek: .ajan, isim: "Agu Kalkanı", font: .title, textfont: .system(size: 7))
+            .padding(10)
     }
 }
