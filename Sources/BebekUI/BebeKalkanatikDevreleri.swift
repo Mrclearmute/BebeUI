58e3b089ff821001264d824ac60ee3d5ca798e90
mrclearmute <mrclearmute1@gmail.com>
2023-03-01 20:20:05 +0100

    Birkaç renk ve neşe eklendi.

--- Sources/BebekUI/BebeKalkanatikDevreleri.swift
+++ Sources/BebekUI/BebeKalkanatikDevreleri.swift
@@ -9,11 +9,17 @@ import SwiftUI
 
 @available(macOS 13.0, iOS 16.0, tvOS 16.0, watchOS 8.0, *)
 @frozen public struct BebeKalkanatikDevreleri: View {
+    @State private var güvenli = true
     public var body: some View {
         NavigationStack {
-            Image(systemName: "lock.trianglebadge.exclamationmark")
-                .symbolVariant(.circle.slash)
-                .foregroundStyle(.yellow, .green)
+            if güvenli == false{
+                Image(systemName: "lock.trianglebadge.exclamationmark")
+                    .symbolVariant(.circle.slash)
+                    .foregroundStyle(.yellow, .red)
+            } else {
+                Image("custom.lock.shield", bundle: .module)
+                    .foregroundStyle(.green)
+            }
         }
     }
     @inlinable public init(){
