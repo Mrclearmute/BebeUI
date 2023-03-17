b432c178cc575639f5e663383f29a47f529918b2
mrclearmute <mrclearmute1@gmail.com>
2023-03-17 21:04:16 +0100

    Changes

---
+++ Sources/BebekUI/BebeKalkannatikdevreleri.swift
@@ -0,0 +1,22 @@
+//
+//  BebeKalkanatikDevreleri.swift
+//  
+//
+//  Created by Sinan Caliskan on 2023-03-03.
+//
+
+import SwiftUI
+
+@available(macOS 13.0, iOS 16.0, tvOS 16.0, watchOS 8.0, *)
+struct SwiftUIView_2: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+@available(macOS 13.0, iOS 16.0, tvOS 16.0, watchOS 8.0, *)
+struct SwiftUIView2Previews: PreviewProvider {
+    static var previews: some View {
+        SwiftUIView_2()
+    }
+}
